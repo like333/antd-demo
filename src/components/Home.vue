a42e51768ab5794b85e57dbeee8df5bd33b40e82
like333 <1046751649@qq.com>
2019-09-19 17:09:30 +0800

    菜单完善

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <a-layout id="components-layout-demo-responsive">
-    <a-layout-sider
-      breakpoint="lg"
-      collapsedWidth="0"
-      @collapse="onCollapse"
-      @breakpoint="onBreakpoint"
-    >
+    <a-layout-sider :trigger="null" collapsible v-model="collapsed">
       <div class="logo" />
       <a-menu theme="dark" mode="inline" :defaultSelectedKeys="['1']" :selectedKeys="[selKey]">
         <a-menu-item key="1" @click="menuClick">
@@ -57,7 +52,99 @@
       </a-menu>
     </a-layout-sider>
     <a-layout>
-      <a-layout-header :style="{ background: '#fff', padding: 0 }" />
+      <a-layout-header :style="{ background: '#fff', padding: 0 }">
+        <a-row type="flex" justify="space-between" align="middle">
+          <a-col>
+            <a-icon
+              class="trigger"
+              :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+              @click="()=> collapsed = !collapsed"
+            />
+          </a-col>
+          <a-col class="tool-group">
+            <a-icon class="tool-item search" type="search" />
+            <a-tooltip placement="bottom">
+              <template slot="title">
+                <span>prompt text</span>
+              </template>
+              <span class="tool-item">
+                <a-icon type="question-circle" />
+              </span>
+            </a-tooltip>
+            <a-dropdown placement="bottomRight" :trigger="['click']">
+              <span class="tool-item">
+                <a-badge :count="9" :overflowCount="99" :offset="[5,-3]">
+                  <a-icon type="bell" />
+                </a-badge>
+              </span>
+              <a-menu slot="overlay" >
+                <a-menu-item>
+                <a-tabs defaultActiveKey="1" @change="callback">
+                  <a-tab-pane tab="通知" key="1">
+                    <!-- <a-list itemLayout="horizontal" :dataSource="notification">
+                      <a-list-item slot="renderItem" slot-scope="item, index">
+                        <a-list-item-meta :description="item.date">
+                          <a slot="title" href="#">{{item.title}}</a>
+                          <a-avatar slot="avatar" src="#" />
+                        </a-list-item-meta>
+                      </a-list-item>
+                    </a-list> -->
+                    Content of Tab Pane 1
+                  </a-tab-pane>
+                  <a-tab-pane tab="消息" key="2" >
+                    <!-- <a-list itemLayout="horizontal" :dataSource="data">
+                      <a-list-item slot="renderItem" slot-scope="item, index">
+                        <a-list-item-meta :description="{{item.description}}">
+                          <a slot="title" href="#">{{item.title}}</a>
+                          <a-avatar slot="avatar" src="#" />
+                        </a-list-item-meta>
+                      </a-list-item>
+                    </a-list> -->
+                    Content of Tab Pane 1
+                  </a-tab-pane>
+                 <a-tab-pane tab="待办" key="3" >
+                    <!-- <a-list itemLayout="horizontal" :dataSource="data">
+                      <a-list-item slot="renderItem" slot-scope="item, index">
+                        <a-list-item-meta :description="{{item.description}}">
+                          <a slot="title" href="#">{{item.title}}</a>
+                          <a-avatar slot="avatar" src="#" />
+                        </a-list-item-meta>
+                      </a-list-item>
+                    </a-list> -->
+                    Content of Tab Pane 3
+                  </a-tab-pane>
+                </a-tabs>
+                </a-menu-item>
+              </a-menu>
+            </a-dropdown>
+            <a-dropdown placement="bottomRight">
+              <span class="tool-item">
+                <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                <span class="userName">Serati Ma</span>
+              </span>
+              <a-menu slot="overlay">
+                <a-menu-item key="0">
+                  <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+                    <a-icon type="user" />个人中心
+                  </a>
+                </a-menu-item>
+
+                <a-menu-item key="1">
+                  <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+                    <a-icon type="setting" />设置
+                  </a>
+                </a-menu-item>
+                <a-menu-divider />
+                <a-menu-item key="2">
+                  <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+                    <a-icon type="poweroff" />退出登录
+                  </a>
+                </a-menu-item>
+              </a-menu>
+            </a-dropdown>
+          </a-col>
+        </a-row>
+      </a-layout-header>
       <a-layout-content :style="{ margin: '24px 16px 0' }">
         <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
           <router-view></router-view>
@@ -68,11 +155,85 @@
   </a-layout>
 </template>
 <script>
-export default {
-  data(){
-    return{
-      selKey:''
+const data = {
+  notification: [
+    {
+      title: "你收到了14份新周报",
+      date: "2017-9-12",
+      iconStyle: ""
+    },
+    {
+      title: "你推荐的 曲妮妮 已通过第三轮面试",
+      date: "2017-9-12",
+      iconStyle: ""
+    },
+    {
+      title: "这种模板可以区分多种通知类型",
+      date: "2017-9-12",
+      iconStyle: ""
+    },
+    {
+      title: "左侧图标用于区分不同的类型",
+      date: "2017-9-12",
+      iconStyle: ""
+    },
+    {
+      title: "内容不要超过两行字，超出时自动截断",
+      date: "2017-9-12",
+      iconStyle: ""
     }
+  ],
+  message: [
+    {
+      title: "曲丽丽 评论了你",
+      description: "描述信息描述信息描述信息",
+      date: "2017-9-12",
+      avatar: ""
+    },
+    {
+      title: "朱偏右 回复了你",
+      description: "这种模板用于提醒谁与你发生了互动，左侧放[谁]的头像",
+      date: "2017-9-12",
+      avatar: ""
+    },
+    {
+      title: "曲丽丽 评论了你",
+      description: "这种模板用于提醒谁与你发生了互动，左侧放[谁]的头像",
+      date: "2017-9-12",
+      avatar: ""
+    }
+  ],
+  todoList: [
+    {
+      title: "任务名称",
+      description: "任务需要在2017-01-12 20:00 前启动",
+      state: "未开始"
+    },
+    {
+      title: "第三方紧急代码变更",
+      description: "冠霖提交于 2017-01-06，需在 2017-01-07 前完成代码变更任务",
+      state: "马上到期"
+    },
+    {
+      title: "信息安全考试",
+      description: "指派竹尔 2017-01-09 前完成更新并发布",
+      state: "已耗时8天"
+    },
+    {
+      title: "ABCD版本发布",
+      description: "冠霖提交于 2017-01-06，需在 2017-01-07 前完成代码变更任务",
+      state: "进行中"
+    }
+  ]
+};
+
+export default {
+  data() {
+    return {
+      selKey: "",
+      collapsed: false,
+      data:data,
+    };
   },
   methods: {
     onCollapse(collapsed, type) {
@@ -81,9 +242,10 @@ export default {
     onBreakpoint(broken) {
       console.log(broken);
     },
+
     menuClick(item, key, keyPath) {
-      this.selKey = item.key
-      console.log("key:",item.key)
+      this.selKey = item.key;
+      console.log("key:", item.key);
       switch (item.key) {
         case "1":
           this.$router.push("workBench");
@@ -128,18 +290,50 @@ export default {
           this.$router.push("monitor");
           break;
       }
+    },
+    callback(key) {
+      console.log(key);
     }
   },
-  mounted:function(){
-   this.selKey = '1'
-  },
+  mounted: function() {
+    this.selKey = "1";
+  }
 };
 </script>
 
 <style>
 #components-layout-demo-responsive .logo {
   height: 32px;
-  background: rgba(255, 255, 255, 0.2);
+  background: url(../assets/menu_logo.svg) 9px / auto 28px no-repeat;
   margin: 16px;
+}
+.ant-layout-sider {
+  height: 100vh;
+}
+#components-layout-demo-responsive .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+#components-layout-demo-responsive .trigger:hover {
+  color: #1890ff;
+}
+#components-layout-demo-responsive .tool-group {
+  padding-right: 24px;
+}
+
+#components-layout-demo-responsive .tool-group .tool-item {
+  height: 100%;
+  display: inline-block;
+  padding: 0 14px;
+  cursor: pointer;
+}
+#components-layout-demo-responsive .tool-group span:hover {
+  background-color: rgba(0, 0, 0, 0.02);
+}
+#components-layout-demo-responsive .tool-group .userName {
+  padding-left: 10px;
 }
 </style>
